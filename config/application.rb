4be1e96a8f58a743fa3ce9f07af5c746c471ff8b
Mark Bussey <mark@curationexperts.com>
2018-04-19 22:17:02 -0500

    Run background jobs inline

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,11 @@ module HyraxDemo
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
+    # Process background jobs inline
+    # You will want to change this for production and may want to move this setting
+    # to config/enfiornments/development.rb
+    config.active_job.queue_adapter = :inline
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
