6ab73f2d6767134a12125201d595921e99b1f0b9
Tom Johnson <tom@dp.la>
2018-04-24 08:50:04 -0700

    Add failing tests for `#year` in work show and presenter

--- app/presenters/hyrax/work_presenter.rb
+++ app/presenters/hyrax/work_presenter.rb
@@ -1,6 +1,5 @@
-# Generated via
-#  `rails generate hyrax:work Work`
 module Hyrax
   class WorkPresenter < Hyrax::WorkShowPresenter
+    delegate :year, to: :solr_document
   end
 end
--- spec/presenters/hyrax/work_presenter_spec.rb
+++ spec/presenters/hyrax/work_presenter_spec.rb
@@ -1,9 +1,23 @@
-# Generated via
-#  `rails generate hyrax:work Work`
 require 'rails_helper'
 
 RSpec.describe Hyrax::WorkPresenter do
-  it "has tests" do
-    skip "Add your tests here"
+  subject(:presenter) { described_class.new(SolrDocument.new(work.to_solr), nil) }
+
+  let(:title)      { ['Journey to Skull Island'] }
+  let(:creator)    { ['Quest, Jane'] }
+  let(:keyword)    { ['Pirates', 'Adventure'] }
+  let(:visibility) { Hydra::AccessControls::AccessRight::VISIBILITY_TEXT_VALUE_PUBLIC }
+  let(:user)       { 'test@example.com' }
+  let(:year)       { ['2018'] }
+
+  let :work do
+    Work.new(title:      title,
+             creator:    creator,
+             keyword:    keyword,
+             year:       year,
+             visibility: visibility,
+             depositor:  user)
   end
+
+  it { is_expected.to have_attributes(title: title, year: year) }
 end
